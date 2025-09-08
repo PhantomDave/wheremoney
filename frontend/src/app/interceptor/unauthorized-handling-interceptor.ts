c3bfe55f505a63606756d7b7373e127a54c31285
Davide Rodo <davide.rodo@baxenergy.com>
2025-09-08 21:50:48 +0200

    Add import functionality with file upload and routing enhancements

--- frontend/src/app/interceptor/unauthorized-handling-interceptor.ts
+++ frontend/src/app/interceptor/unauthorized-handling-interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpInterceptorFn, HttpClient } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
--- frontend/src/app/services/api-wrapper.ts
+++ frontend/src/app/services/api-wrapper.ts
@@ -1,3 +1,5 @@
+// noinspection ExceptionCaughtLocallyJS
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
